c788d5a0c67902ac20d742cb44bf9618cf71acf0
Ren-Yaqian <74707036+Ren-Yaqian@users.noreply.github.com>
2021-08-12 15:21:18 +0800

    Add files via upload

---
+++ src/main/java/com/peixinchen/model/Album.java
@@ -0,0 +1,8 @@
+package com.peixinchen.model;
+
+public class Album {
+    public Integer aid;
+    public String name;
+    public String cover;
+    public Integer count;
+}
---
+++ src/main/java/com/peixinchen/model/User.java
@@ -0,0 +1,7 @@
+package com.peixinchen.model;
+
+public class User {
+    public Integer uid;
+    public String username;
+    public String nickname;
+}
